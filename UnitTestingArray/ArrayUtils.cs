7253bc198d33f784889fc3ff5070eb7f646b62c3
porteno12 <111998600+porteno12@users.noreply.github.com>
2025-07-29 09:06:01 +0300

    Add files via upload

---
+++ UnitTestingArray/ArrayUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingArray
+{
+    public class ArrayUtils
+    {
+        
+        public static bool IsAmazing3(int[] arr)
+        {
+            // על התלמיד להשלים כאן את המימוש
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTestingArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingArray
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int[] test1 = { 3, 6, 9 };
+                Console.WriteLine($"Result for test1: {ArrayUtils.IsAmazing3(test1)}");
+
+                int[] test2 = { 2, 4, 7 };
+                Console.WriteLine($"Result for test2: {ArrayUtils.IsAmazing3(test2)}");
+
+                Console.WriteLine("Done.");
+            }
+            catch (NotImplementedException ex)
+            {
+                Console.WriteLine($"Caught Exception: {ex.GetType().Name} - {ex.Message}");
+            }
+        }
+
+    }
+}
